aab3f1ee758ffd416c57abeb5145c60768a45804
Em Szmadzinski <smujmaiku@gmail.com>
2026-02-12 22:06:08 -0500

    more real tests

--- src/mod.test.ts
+++ src/mod.test.ts
@@ -1,6 +1,6 @@
 import { afterEach, describe, it } from '@std/testing/bdd';
 import { FakeTime } from '@std/testing/time';
-import { expect, fn } from '@std/expect';
+import { expect } from '@std/expect';
 import TTLStore from './mod.ts';
 
 describe('TTLStore', () => {
@@ -16,5 +16,20 @@ describe('TTLStore', () => {
 
 		store.setItem('one', 'mock1');
 		expect(store.getItem('one')).toEqual('mock1');
+	});
+
+	it('should expire state', async () => {
+		using mockTime = new FakeTime();
+
+		const store = new TTLStore('mock', localStorage);
+
+		store.setItem('one', 'mock1', 10);
+		await mockTime.tickAsync(9_000);
+		expect(store.getItem('one')).toEqual('mock1');
+		expect(localStorage.length).toBe(2);
+
+		await mockTime.tickAsync(2_000);
+		expect(store.getItem('one')).toBeNull();
+		expect(localStorage.length).toBe(1);
 	});
 });
